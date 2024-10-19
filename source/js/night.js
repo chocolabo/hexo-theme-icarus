e9d434ac21739dc453b357704be80d32057ce960
chocolabo <hiddymeister@gmail.com>
2024-10-19 18:30:06 +0900

    修正

--- source/js/night.js
+++ source/js/night.js
@@ -1,15 +1,15 @@
 (function () {
     /**
+  
      * Icarus 夜间模式 by iMaeGoo
      * https://www.imaegoo.com/
-     */
-
-    var isNight = localStorage.getItem('night');
-    var nightNav;
-
-    // ナイトモードを適用する関数
+        */
+  
+      var isNight = localStorage.getItem('night');
+      var nightNav;
+  
     function applyNight(value) {
-        if (value && value.toString() === 'true') {
+        if (value.toString() === 'true') {
             document.body.classList.remove('light');
             document.body.classList.add('night');
         } else {
@@ -17,8 +17,7 @@
             document.body.classList.add('light');
         }
     }
-
-    // ナイトモード切り替えボタンを探す
+  
     function findNightNav() {
         nightNav = document.getElementById('night-nav');
         if (!nightNav) {
@@ -27,27 +26,14 @@
             nightNav.addEventListener('click', switchNight);
         }
     }
-
-    // ナイトモードの切り替え処理
+  
     function switchNight() {
         isNight = isNight ? isNight.toString() !== 'true' : true;
         applyNight(isNight);
         localStorage.setItem('night', isNight);
     }
-
-    // ページロード前にナイトモードを適用
-    if (isNight && isNight.toString() === 'true') {
-        document.body.classList.add('night');
-    } else {
-        document.body.classList.add('light');
-    }
-
-    // ページロード時にナイトモードを適用し、ページを表示
-    document.addEventListener('DOMContentLoaded', function() {
-        findNightNav();
-        applyNight(isNight);
-
-        // ページを表示するためのクラスを追加
-        document.body.classList.add('loaded');
-    });
-}());
+  
+    findNightNav();
+    isNight && applyNight(isNight);
+  }());
+  
